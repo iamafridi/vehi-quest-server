28d4a42c703b1966f5547f3118b1fa8b2d6694ae
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2024-01-16 18:05:23 +0600

    Implemented Payment Intent API

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const morgan = require("morgan");
 const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 
 // middleware
 const corsOptions = {
@@ -121,11 +122,35 @@ async function run() {
     });
 
     //Save Vehicle for the host
-    app.get('/rooms/:email',async(req,res)=>{
-      const email= req.params.email;
-      const result = await vehiclesCollection.find().toArray();
+    app.get("/rooms/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await vehiclesCollection
+        .find({ "host.email": email })
+        .toArray();
       res.send(result);
-    }) 
+    });
+
+    // ****User Role *********
+    // Get user Role
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email });
+      res.send(result);
+    });
+
+    // ****Payment Intent****
+    // Generating Payment Secret for stripe
+    app.post("create-payment-intent", verifyToken, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      if (!price || amount < 1) return;
+      const { client_secret } = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: client_secret });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
