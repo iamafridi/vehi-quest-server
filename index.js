fa6c315f36c39461ebd9484ca2f29159d0ba04bd
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2024-01-16 20:45:01 +0600

    User Requested API reused

--- index.js
+++ index.js
@@ -88,7 +88,19 @@ async function run() {
       const options = { upsert: true };
       const isExist = await usersCollection.findOne(query);
       console.log("User found?----->", isExist);
-      if (isExist) return res.send(isExist);
+      if (isExist) {
+        if (user?.status === "Requested") {
+          const result = await usersCollection.updateOne(query, {
+            $set: {
+              user,
+            },
+            options,
+          });
+          return res.send(result);
+        } else {
+          return res.send(isExist);
+        }
+      }
       const result = await usersCollection.updateOne(
         query,
         {
@@ -216,6 +228,8 @@ async function run() {
       const result = await usersCollection.updateOne(query, updateDoc, options);
       res.send(result);
     });
+
+    // Request for Become a host
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
