61905c10777004d62124e1bd232ca012f0ea730b
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2024-01-16 20:09:57 +0600

    Update role API created

--- index.js
+++ index.js
@@ -195,6 +195,28 @@ async function run() {
       res.send(result);
     });
 
+    // Get All Users
+    app.get("/users", verifyToken, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Update user Role
+    app.put("/users/update/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now(),
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
